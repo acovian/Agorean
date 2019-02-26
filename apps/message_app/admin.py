8782977ca9827afa1ac96000ca309fec02c43c47
Alex Covian <alejandrocovian3@gmail.com>
2019-02-25 18:05:00 -0800

    added css to profile

--- apps/message_app/admin.py
+++ apps/message_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Message, Comment, Like
+
+admin.site.register(Message)
+admin.site.register(Comment)
+admin.site.register(Like)
