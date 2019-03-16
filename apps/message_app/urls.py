711c022e4cef774ed3c30787f0b274aa16df08d1
Alex Covian <alejandrocovian3@gmail.com>
2019-03-15 21:04:04 -0700

    fixed quotation marks on urls for user_app

--- apps/message_app/urls.py
+++ apps/message_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'message_app'
+app_name = "message_app"
 
 urlpatterns = [
     url(r"^$", views.index, name="index"),
--- apps/user_app/urls.py
+++ apps/user_app/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import url
 from . import views
 
-app_name='user_app'
+app_name="user_app"
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^login$', views.login, name = 'login'),
-    url(r'^register$', views.register, name = 'register'),
-    url(r'^logout$', views.logout, name = 'logout'),
-    url(r'^user(?P<id>\d+)$', views.user, name = 'user'),
-    url(r'^settings/(?P<user_id>\d+)/', views.settings, name='settings'),
-    url(r'^update_information(?P<user_id>\d+)$', views.update_information, name = 'update_information'),
-    url(r'^delete(?P<user_id>\d+)$', views.delete, name = 'delete'),
+    url(r"^$", views.index, name = "index"),
+    url(r"^login$", views.login, name = "login"),
+    url(r"^register$", views.register, name = "register"),
+    url(r"^logout$", views.logout, name = "logout"),
+    url(r"^user(?P<id>\d+)$", views.user, name = "user"),
+    url(r"^settings/(?P<user_id>\d+)/", views.settings, name="settings"),
+    url(r"^update_information(?P<user_id>\d+)$", views.update_information, name = "update_information"),
+    url(r"^delete(?P<user_id>\d+)$", views.delete, name = "delete"),
 ]
